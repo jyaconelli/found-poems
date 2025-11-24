9f3fcee26f7d742d36d4b9610cae5bf8663461d9
Josie Yaconelli <jyaconelli@gmail.com>
2025-11-24 13:22:23 -0800

    NUKE IT

--- api/index.js
+++
@@ -1,5 +0,0 @@
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
--- web/src/socket.js
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client'
-
-const URL = process.env.REACT_APP_NODE_ENV === 'production' ? `${process.env.REACT_APP_API_ADDRESS}:3000` : 'http://10.0.0.43:3000';
-
-export const socket = io(URL)
